a86dfc863e1e535b7be3c0bbb2ab794a92c170ed
pklonowski <paul.klonowski@gmail.com>
2019-05-21 22:41:54 -0600

    To p. 153 - skipping over to ch09 (p.177)

--- test/rumbl/accounts/accounts_test.exs
+++ test/rumbl/accounts/accounts_test.exs
@@ -19,5 +19,34 @@ defmodule Rubl.AccountsTest do
       assert user.credential.email == "eva@test"
       assert [%User{id: ^id}] = Accounts.list_users()
     end
+
+    test "with invalid data does not isert user" do
+      assert {:error, _changeset} = Accounts.register_user(@invalid_attrs)
+      assert Accounts.list_users() == []
+    end
+
+    test "enforces unique usernames" do
+      assert {:ok, %User{id: id}} = Accounts.register_user(@valid_attrs)
+      assert {:error, changeset} = Accounts.register_user(@valid_attrs)
+
+      assert %{username: ["has already been taken"]} = errors_on(changeset)
+
+      assert [%User{id: ^id}] = Accounts.list_users()
+    end
+
+    test "does not accept long usernames" do
+      attrs = Map.put(@valid_attrs, :username, String.duplicate("a", 30))
+      {:error, changeset} = Accounts.register_user(attrs)
+
+      assert %{username: ["should be at most 20 character(s)"]} = errors_on(changeset)
+      assert Accounts.list_users() == []
+    end
+
+    test "requires password to be at least 6 chars long" do
+      attrs = put_in(@valid_attrs, [:credential, :password], "12345")
+      {:error, changeset} = Accounts.register_user(attrs)
+      assert %{password: ["should be at least 6 character(s)"]} = errors_on(changeset)[:credential]
+      assert Accounts.list_users() == []
+    end
   end
 end
